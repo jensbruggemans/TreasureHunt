6a9b4633760496f82eafdd12e819fb0e6d200b78
Jens Bruggemans <jens@embur.be>
2016-04-11 13:06:43 +0200

    First commit of project.

---
+++ TreasureHunt/TreasureHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TreasureHunt
+//
+//  Created by Jens Bruggemans on 11/04/16.
+//  Copyright © 2016 embur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
